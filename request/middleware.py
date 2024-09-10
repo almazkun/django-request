e6fe9a2d2776780ec88ea04befc96b6467e01338
Almaz Kunpeissov <almaz.kun@gmail.com>
2024-09-10 14:47:17 +1000

    Add: AsyncRequestMiddleware, to save in thread

--- request/middleware.py
+++ request/middleware.py
@@ -1,4 +1,5 @@
 import logging
+from concurrent.futures import ThreadPoolExecutor
 
 from django.core.exceptions import ValidationError
 from django.utils.deprecation import MiddlewareMixin
@@ -10,44 +11,68 @@ from .utils import request_is_ajax
 
 logger = logging.getLogger("request.security.middleware")
 
+executor = ThreadPoolExecutor(max_workers=1)
+
+
+def to_process_request(request, response):
+    if request.method.lower() not in settings.VALID_METHOD_NAMES:
+        return False
+
+    if response.status_code < 400 and settings.ONLY_ERRORS:
+        return False
+
+    ignore = Patterns(False, *settings.IGNORE_PATHS)
+    if ignore.resolve(request.path[1:]):
+        return False
+
+    if request_is_ajax(request) and settings.IGNORE_AJAX:
+        return False
+
+    if request.META.get("REMOTE_ADDR") in settings.IGNORE_IP:
+        return False
+
+    ignore = Patterns(False, *settings.IGNORE_USER_AGENTS)
+    if ignore.resolve(request.META.get("HTTP_USER_AGENT", "")):
+        return False
+
+    if getattr(request, "user", False):
+        if request.user.get_username() in settings.IGNORE_USERNAME:
+            return False
+
+    return True
+
+
+def do_save_request(request, response):
+    r = Request()
+    try:
+        r.from_http_request(request, response, commit=False)
+        r.full_clean()
+    except ValidationError as exc:
+        logger.warning(
+            "Bad request: %s",
+            str(exc),
+            exc_info=exc,
+            extra={"status_code": 400, "request": request},
+        )
+    else:
+        r.save()
+
 
 class RequestMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
-        if request.method.lower() not in settings.VALID_METHOD_NAMES:
+        if not to_process_request(request, response):
             return response
 
-        if response.status_code < 400 and settings.ONLY_ERRORS:
+        do_save_request(request, response)
+
+        return response
+
+
+class AsyncRequestMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        if not to_process_request(request, response):
             return response
 
-        ignore = Patterns(False, *settings.IGNORE_PATHS)
-        if ignore.resolve(request.path[1:]):
-            return response
+        executor.submit(do_save_request, request, response)
 
-        if request_is_ajax(request) and settings.IGNORE_AJAX:
-            return response
-
-        if request.META.get("REMOTE_ADDR") in settings.IGNORE_IP:
-            return response
-
-        ignore = Patterns(False, *settings.IGNORE_USER_AGENTS)
-        if ignore.resolve(request.META.get("HTTP_USER_AGENT", "")):
-            return response
-
-        if getattr(request, "user", False):
-            if request.user.get_username() in settings.IGNORE_USERNAME:
-                return response
-
-        r = Request()
-        try:
-            r.from_http_request(request, response, commit=False)
-            r.full_clean()
-        except ValidationError as exc:
-            logger.warning(
-                "Bad request: %s",
-                str(exc),
-                exc_info=exc,
-                extra={"status_code": 400, "request": request},
-            )
-        else:
-            r.save()
         return response
